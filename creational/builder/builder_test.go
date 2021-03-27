da98f0047fd44412269f5a869531639e3944baea
Thanh Tran <tranthanh19589@gmail.com>
2021-03-28 00:47:26 +0700

    add creational patterns

---
+++ creational/builder/builder_test.go
@@ -0,0 +1,26 @@
+package builder
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestBuilderPattern(t *testing.T) {
+	director := ManufacturingDirector{}
+
+	carBuilder := &CarBuilder{}
+	director.SetBuilder(carBuilder)
+	director.Construct()
+	car := carBuilder.Build()
+	if car.Wheels != 4 {
+		t.Errorf("Something went wrong : " + strconv.Itoa(car.Wheels) + " wheels found")
+	}
+
+	biciclettaBuilder := &BiciclettaBuilder{}
+	director.SetBuilder(biciclettaBuilder)
+	director.Construct()
+	bike := biciclettaBuilder.Build()
+	if bike.Wheels != 2 {
+		t.Errorf("Something went wrong : " + strconv.Itoa(bike.Wheels) + " wheels found")
+	}
+}
